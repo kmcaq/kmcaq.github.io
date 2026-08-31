d6e7f394da3e104fca8ba35f9d15ef8862ee3525
kmcaq <km.tamami@gmail.com>
2026-08-31 17:21:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 const components = [
-  ["navbar", "components/navbar.html"],
-  ["hero", "components/hero.html"],
-  ["links", "components/links.html"],
-  ["gameshelf", "components/gameshelf.html"],
-  ["schedule", "components/schedule.html"]
+  ["navbar","components/navbar.html"],
+  ["hero","components/hero.html"],
+  ["links","components/links.html"],
+  ["gameshelf","components/gameshelf.html"],
+  ["schedule","components/schedule.html"]
 ];
 
-async function loadComponents() {
+async function loadComponents(){
 
   await Promise.all(
 
-    components.map(async ([id, file]) => {
+    components.map(async ([id,file])=>{
 
-      const html = await fetch(file, { cache: "no-cache" })
-        .then(r => r.text());
+      const html=await fetch(file,{cache:"no-cache"})
+      .then(r=>r.text());
 
-      document.getElementById(id).innerHTML = html;
+      document.getElementById(id).innerHTML=html;
 
     })
 
   );
 
-  if (window.initLive) initLive();
-  if (window.initNavbar) initNavbar();
-  if (window.initGameshelf) initGameshelf();
+  if(window.initLive) initLive();
+  if(window.initNavbar) initNavbar();
+  if(window.initGameshelf) initGameshelf();
 
 }
 
